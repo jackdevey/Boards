acc513ee2a1f72c348338f65c3021a2d6a71ce75
Jack Devey <jack@jw3.uk>
2024-09-11 13:42:23 +0100

    Fixes

--- index.ts
+++ index.ts
@@ -6,15 +6,17 @@ import ora from "ora";
 dotenv.config();
 
 // Log in to NR api
-const api = new LDBWSClient(process.env.TOKEN!!)
+const api = new LDBWSClient(process.env.TOKEN!!);
 
 // Get the station code args
-const crs = process.argv[2].toUpperCase();
+process.argv.shift();
+process.argv.shift();
+const crs = process.argv.join().toUpperCase();
 
 // Get the number of services
 if (process.argv.length == 4) {
-    var num = process.argv[3]
-} else num = 10
+  var num = process.argv[3];
+} else num = 10;
 
 // Clear the terminal before printing
 print("\x1Bc");
@@ -24,87 +26,97 @@ const spinner = ora("Loading departure board for " + crs).start();
 
 // Update the data every 30 seconds
 async function update() {
-    // Move the cursor to remove the new line
-    setTimeout(async function () {
-        // Get the departures from NR
-        var resp = await api.GetDepartureBoard(crs, num);
-        // Move the cursor to write over the previous table
-        print("\x1B[5;0H");
-        // Print the data
-        printBody(resp);
-        // Live updates
-        update();
-    }, 30000);
-}
-
-// Get the initial data & then begin updates
-const get = async () => {
+  // Move the cursor to remove the new line
+  setTimeout(async function () {
     // Get the departures from NR
-    try {
-        var resp = await api.GetDepartureBoard(crs, num);
-    } catch(err) {
-        // If an error occured
-        print(chalk.red("\nError: " + err.message));
-        return
-    }
-    // Stop spinning & clear the terminal 
-    // to remove loading message
-    spinner.stop();
-    print("\x1Bc");
-    // Print the title block
-    title("BHM");
-    // Print the table headings
-    print(chalk.bold(formatString("Time", 5)));
-    print(chalk.bold(formatString("Destination", 25)));
-    print(chalk.bold(formatString("Plat", 4)));
-    print(chalk.bold(formatString("Expected", 11)));
-    print(chalk.bold(formatString("Operator", 30)) + "\n");
+    var resp = await api.GetDepartureBoard(crs, num);
+    // Move the cursor to write over the previous table
+    print("\x1B[5;0H");
     // Print the data
     printBody(resp);
     // Live updates
     update();
+  }, 30000);
 }
 
-get()
+// Get the initial data & then begin updates
+const get = async () => {
+  // Get the departures from NR
+  try {
+    var resp = await api.GetDepartureBoard(crs, num);
+  } catch (err) {
+    // If an error occured
+    print(chalk.red("\nError: " + err.message));
+    return;
+  }
+  // Stop spinning & clear the terminal
+  // to remove loading message
+  spinner.stop();
+  print("\x1Bc");
+  // Print the title block
+  title("BHM");
+  // Print the table headings
+  print(chalk.bold(formatString("Time", 5)));
+  print(chalk.bold(formatString("Destination", 25)));
+  print(chalk.bold(formatString("Plat", 4)));
+  print(chalk.bold(formatString("Expected", 11)));
+  print(chalk.bold(formatString("Operator", 30)) + "\n");
+  // Print the data
+  printBody(resp);
+  // Live updates
+  update();
+};
+
+get();
 
 // Print the main body of data from the response
 function printBody(resp) {
-    resp.trainServices.service.forEach(departure => {
-        // If there isn't a platform, set it to "-"
-        if (departure.platform != null) {
-            var platform = departure.platform
-        } else platform = "-"
-        // Calculate formatting for the expected time
-        var expected
-        if (departure.etd == "On time") expected = chalk.green(formatString("✓ " + departure.etd, 11))
-        else if (departure.etd == "Cancelled") expected = chalk.red(formatString("✗ " + departure.etd, 11))
-        else expected = chalk.yellow(formatString("! " + departure.etd, 11))
-        // Print the row out
-        print(chalk.gray(formatString(departure.std, 5)))
-        print(chalk.white(formatString(departure.destination.location[0].locationName, 25)))
-        print(chalk.grey(formatString(platform, 4)))
-        print(expected)
-        print(chalk.grey(formatString(departure.operator, 30)))
-        print("\n") // New line for next row
-    });
+  resp.trainServices.service.forEach((departure) => {
+    // If there isn't a platform, set it to "-"
+    if (departure.platform != null) {
+      var platform = departure.platform;
+    } else platform = "-";
+    // Calculate formatting for the expected time
+    var expected;
+    if (departure.etd == "On time")
+      expected = chalk.green(formatString("✓ " + departure.etd, 11));
+    else if (departure.etd == "Cancelled")
+      expected = chalk.red(formatString("✗ " + departure.etd, 11));
+    else expected = chalk.yellow(formatString("! " + departure.etd, 11));
+    // Print the row out
+    print(chalk.gray(formatString(departure.std, 5)));
+    print(
+      chalk.white(
+        formatString(departure.destination.location[0].locationName, 25)
+      )
+    );
+    print(chalk.grey(formatString(platform, 4)));
+    print(expected);
+    print(chalk.grey(formatString(departure.operator, 30)));
+    print("\n"); // New line for next row
+  });
 }
 
 // Prin the title block
 function title() {
-    print(chalk.redBright("LIVE") + chalk.white(" Departures from " + crs + "\n")) // Station code
-    print(chalk.gray("Powered by National Rail Enquiries\n")) // NR Attribution
-    console.log() // New line for spacing
+  print(
+    chalk.redBright("LIVE") + chalk.white(" Departures from " + crs + "\n")
+  ); // Station code
+  print(chalk.gray("Powered by National Rail Enquiries\n")); // NR Attribution
+  console.log(); // New line for spacing
 }
 
 // Wrapper function for printing without new line
-function print(string) { process.stdout.write(string); }
+function print(string) {
+  process.stdout.write(string);
+}
 
 // Format a string by adding spaces to the end
 function formatString(string, maxLength) {
-    var spaces = maxLength - string.length;
-    var result = string;
-    for (var i = 0; i < spaces + 2; i++) {
-        result += " "
-    }
-    return result
+  var spaces = maxLength - string.length;
+  var result = string;
+  for (var i = 0; i < spaces + 2; i++) {
+    result += " ";
+  }
+  return result;
 }
